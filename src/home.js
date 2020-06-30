eecd0fb979c13cbd5d54a5d2b8d24c4eb13b4534
thomaz <thomazsousa@outlook.com.br>
2020-06-30 09:55:38 -0300

    create sreen for item type

--- src/home.js
+++ src/home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from './styles';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 const menu = require('./menu.json');
 
 export function HomeScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.itemType}>
+            <TouchableOpacity style={styles.itemType} onPress={()=>{navigation.navigate('ItemList')}}>
                 <Image source={require('./icons_not_free/hamburguer.png')} alt="hamburguer" style={styles.foodIcons}/>
                 <Text style={styles.itemName}>Hamburgers</Text>
             </TouchableOpacity>
@@ -24,3 +23,35 @@ export function HomeScreen({ navigation }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex: 1,
+        backgroundColor: "#111",
+        justifyContent: "space-evenly",
+        alignItems: "center"
+    },
+
+    itemType:{
+        height: 128,
+        width: "80%",
+        borderWidth: 2,
+        borderRadius: 3,
+        borderColor: "#555",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 24
+    },
+
+    foodIcons:{
+        height: 96,
+        width: 96
+    },
+
+    itemName:{
+        color: "#fff",
+        fontSize: 18
+    }
+});
--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {HomeScreen} from './home';
+import {ItemList} from './itemlist';
 
 const Stack = createStackNavigator();
-
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{title: "Bem vindo ao Cannibal Hamburger!", ...header}}/>
-        {/* <Stack.Screen name="Hamburguer" component={DetailsScreen} />
-        <Stack.Screen name="Sushi" component={DetailsScreen} />
-        <Stack.Screen name="Bebidas" component={DetailsScreen} /> */}
+        <Stack.Screen name="ItemList" component={ItemList} options={{title: "Escolha os items:", ...header}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
